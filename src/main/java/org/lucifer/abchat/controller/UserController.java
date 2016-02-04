73492b17a2553e523b37fd199adec0ed68928d61
Igor <lucifer-angelok@mail.ru>
2016-02-04 17:36:57 +0200

    Incomplete registration

--- src/main/java/org/lucifer/abchat/controller/UserController.java
+++ src/main/java/org/lucifer/abchat/controller/UserController.java
@@ -1,9 +1,14 @@
 package org.lucifer.abchat.controller;
 
+import org.lucifer.abchat.domain.User;
 import org.lucifer.abchat.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * Created by Игорь on 19.06.2015.
@@ -14,4 +19,27 @@ public class UserController {
     @Autowired
     PersonService personService;
 
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public @ResponseBody String register(User user) {
+        List<User> users = personService.getAll();
+        for (User u : users) {
+            if (u.getLogin().equals(user.getLogin())) {
+                return "Error";
+            }
+        }
+        personService.save(user);
+        return "Ok";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public @ResponseBody String login(User user) {
+        List<User> users = personService.getAll();
+        for (User u : users) {
+            if (u.getLogin().equals(user.getLogin())
+                    && u.getPassword().equals(user.getPassword())) {
+                return "Ok";
+            }
+        }
+        return "Error";
+    }
 }
--- src/main/java/org/lucifer/abchat/domain/User.java
+++ src/main/java/org/lucifer/abchat/domain/User.java
@@ -13,16 +13,67 @@ import java.util.Set;
 @Table(name = "USER")
 public class User extends Identificator implements Serializable{
 
-	@Column(name = "NAME")
-	private String name;
+	@Column(name = "LOGIN")
+	private String login;
 
-	public User(String name, String surname){
-		this.name = name;
-	}
+	@Column(name = "PASSWORD")
+	private String password;
+
+	@Column(name = "EMAIL")
+	private String email;
 
 	public User(){
 
 	}
 
-	//TODO equals and hashcode
+	public User(String login, String password, String email) {
+		this.login = login;
+		this.password = password;
+		this.email = email;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		User user = (User) o;
+
+		if (!login.equals(user.login)) return false;
+		if (!password.equals(user.password)) return false;
+		return email.equals(user.email);
+
+	}
+
+	@Override
+	public int hashCode() {
+		int result = login.hashCode();
+		result = 31 * result + password.hashCode();
+		result = 31 * result + email.hashCode();
+		return result;
+	}
 }
